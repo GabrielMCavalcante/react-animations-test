573424863d2ee47d3fe4a0b74879c17a3b7299be
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-18 12:51:19 -0300

    Added Modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { CSSTransition } from 'react-transition-group'
+import './Modal.css'
+
+const modal = (props) => {
+    return (
+        <CSSTransition
+            in={props.show}
+            timeout={400}
+            mountOnEnter
+            unmountOnExit
+            classNames={{
+                enterActive: 'Open',
+                exitActive: 'Close'
+            }}
+        >
+            <div className="Modal">
+                <h1>A Modal</h1>
+                <button
+                    className="Button"
+                    onClick={props.closed}
+                >Dismiss</button>
+            </div>
+        </CSSTransition>
+    )
+}
+
+export default modal
